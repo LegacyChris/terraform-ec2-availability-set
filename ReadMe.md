cb805507e4345e4f669f23403862b16156fa6e9a
Christian Chime <legacchris@gmail.com>
2019-11-13 04:59:46 -0600

    Initial Commit

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+## To run this template using terraform, go through the steps below:
+
+1. Install Terraform.
+2. Set your AWS credentials as the environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY.
+3. cd into one of the example folders.
+4. Run terraform init.
+5. Run terraform apply.
+6. After it's done deploying, the example will output URLs or IPs you can try out.
+7. To clean up and delete all resources after you're done, run terraform destroy.
---
+++ main.tf
@@ -0,0 +1,54 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# DEPLOY A SINGLE EC2 INSTANCE
+# 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ----------------------------------------------------------------------------------------------------------------------
+# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
+# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
+# ----------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  required_version = ">= 0.12"
+}
+
+# ------------------------------------------------------------------------------
+# CONFIGURE OUR AWS CONNECTION
+# ------------------------------------------------------------------------------
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "aws_availability_zones" "all" {}
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY A SINGLE EC2 INSTANCE
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_instance" "example" {
+  # Ubuntu Server 18.04 LTS (HVM), SSD Volume Type in us-east-2
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE THE SECURITY GROUP THAT'S APPLIED TO THE EC2 INSTANCE
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
